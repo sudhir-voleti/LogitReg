337b62a720d81ef6a034aa161af25504ba0730ce
Shivani Srivastava <shivani141296@gmail.com>
2021-11-25 11:46:49 +0530

    Update dependency.R
    Add library RPART

--- dependency.R
+++ dependency.R
@@ -6,6 +6,7 @@ if(!require("ggplot2")){install.packages("ggplot2")}
 if(!require("DT")){install.packages("DT")}
 if(!require("nnet")){install.packages("nnet")}
 if(!require("reshape2")){install.packages("reshape2")}
+if(!require("rpart")){install.packages("rpart")}
 
 library(tidyverse)
 library(DT)
@@ -15,4 +16,4 @@ library(aod)
 library(foreign)
 library(nnet)
 library(reshape2)
-
+library(rpart)
